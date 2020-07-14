fb96048a82024de9f59d5b4b4f10aa7619521ac1
Kourosh Ghadiri Zahrani <kouroshgz@gmail.com>
2020-07-13 21:29:05 -0700

    Added support for private profiles

--- steamavatardownloader.py
+++ steamavatardownloader.py
@@ -2,11 +2,27 @@ from urllib import request as req
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
-# get profile id
+# get profile id and privacy settings
 print("https://steamcommunity.com/id/[THIS PART IS WHAT YOU ENTER]")
 print('Check for spelling / case mistakes!')
 print('Avatar will be downloaded to folder directory containing steamavatardownloader.py')
 profileID = input("Enter steam profile id: ")
+isPrivate = input("Profile Private: Y/N?")
+
+def f(isPrivate):
+    return {
+            "y": True,
+            "Y": True,
+            "n": False,
+            "N": False,
+        }.get(isPrivate, False)
+
+
+private = f(isPrivate)
+
+
+
+
 # variable holding desired URL
 myUrl = "https://steamcommunity.com/id/" + profileID
 # Open Connection + Grab page
@@ -18,17 +34,29 @@ uClient.close()
 # Parse html
 pageSoup = soup(pageHtml, "html.parser")
 # locate profile avatar url
-if pageSoup.find('meta', property="og:image"):
-    avatarUrl = pageSoup.find('meta', property="og:image")['content']
-    # create substring containing avatar file name
-    file = avatarUrl.split("/avatars/")
-    avatar = file[1][3:]
-    # download profile avatar
-    req.urlretrieve(avatarUrl, avatar)
-    print("Avatar successfully downloaded")
+
+if not private:
+    if pageSoup.find('meta', property="og:image"):
+        avatarUrl = pageSoup.find('meta', property="og:image")['content']
+        # create substring containing avatar file name
+        file = avatarUrl.split("/avatars/")
+        avatar = file[1][3:]
+        # download profile avatar
+        req.urlretrieve(avatarUrl, avatar)
+if private:
+    if pageSoup.find('div', {"class":"playerAvatarAutoSizeInner"}):
+        avatarUrl = pageSoup.find('div', {"class":"playerAvatarAutoSizeInner"}).find('img')['src']
+        # create substring containing avatar file name
+        file = avatarUrl.split("/avatars/")
+        avatar = file[1][3:]
+        # download profile avatar
+        req.urlretrieve(avatarUrl, avatar)
+
+
 else: # messages if profile/avatar cant be located
-    print("Could not find steam profile/avatar")
+    print("Could not find steam profile/avatar or you mistyped Y/N response")
     print("Check your spelling and run the script again")
+
 
 
 
