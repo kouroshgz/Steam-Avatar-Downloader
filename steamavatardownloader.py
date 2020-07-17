675003a65f4dc8b95f7efc4306f6937db22b6df3
Kourosh Ghadiri Zahrani <kouroshgz@gmail.com>
2020-07-16 17:16:34 -0700

    Added support for acquiring multiple avatars without restarting the script, reduced code redundancy, fixed bug relating to steam profiles with avatar borders. No longer need to specify if profile is private or not.

--- steamavatardownloader.py
+++ steamavatardownloader.py
@@ -6,56 +6,45 @@ from bs4 import BeautifulSoup as soup
 print("https://steamcommunity.com/id/[THIS PART IS WHAT YOU ENTER]")
 print('Check for spelling / case mistakes!')
 print('Avatar will be downloaded to folder directory containing steamavatardownloader.py')
-profileID = input("Enter steam profile id: ")
-isPrivate = input("Profile Private: Y/N?")
+repeat = True
 
-def f(isPrivate):
+def dict(repeat):
     return {
             "y": True,
             "Y": True,
             "n": False,
             "N": False,
-        }.get(isPrivate, False)
+        }.get(repeat, False)
+
+while repeat == True:
+	profileID = input("Enter steam profile id: ")
+	# variable holding desired URL
+	myUrl = "https://steamcommunity.com/id/" + profileID
+	# Open Connection + Grab page
+	uClient = uReq(myUrl)
+	# Variable stores raw html
+	pageHtml = uClient.read()
+	# Close Connection
+	uClient.close()
+	# Parse html
+	pageSoup = soup(pageHtml, "html.parser")
+	# locate profile avatar url
+	if pageSoup.find('div', {"class":"playerAvatarAutoSizeInner"}):
+		avatarUrl = pageSoup.find('div', {"class":"playerAvatarAutoSizeInner"}).find('img', recursive = False)['src']
+		# create substring containing avatar file name
+		print(avatarUrl)
+		file = avatarUrl.split("/avatars/")
+		avatar = file[1][3:]
+		# download profile avatar
+		req.urlretrieve(avatarUrl, avatar)
+	else: # messages if profile/avatar cant be located
+		print("Could not find steam profile/avatar or you mistyped Y/N response")
+		print("Check your spelling and run the script again")
+	# Prompt another profile search
+	repeat = input("Search for another profile? (Y/N):" )
+	repeat = dict(repeat)
 
 
-private = f(isPrivate)
-
-
-
-
-# variable holding desired URL
-myUrl = "https://steamcommunity.com/id/" + profileID
-# Open Connection + Grab page
-uClient = uReq(myUrl)
-# Variable stores raw html
-pageHtml = uClient.read()
-# Close Connection
-uClient.close()
-# Parse html
-pageSoup = soup(pageHtml, "html.parser")
-# locate profile avatar url
-
-if not private:
-    if pageSoup.find('meta', property="og:image"):
-        avatarUrl = pageSoup.find('meta', property="og:image")['content']
-        # create substring containing avatar file name
-        file = avatarUrl.split("/avatars/")
-        avatar = file[1][3:]
-        # download profile avatar
-        req.urlretrieve(avatarUrl, avatar)
-if private:
-    if pageSoup.find('div', {"class":"playerAvatarAutoSizeInner"}):
-        avatarUrl = pageSoup.find('div', {"class":"playerAvatarAutoSizeInner"}).find('img')['src']
-        # create substring containing avatar file name
-        file = avatarUrl.split("/avatars/")
-        avatar = file[1][3:]
-        # download profile avatar
-        req.urlretrieve(avatarUrl, avatar)
-
-
-else: # messages if profile/avatar cant be located
-    print("Could not find steam profile/avatar or you mistyped Y/N response")
-    print("Check your spelling and run the script again")
 
 
 
